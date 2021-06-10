dbfb4c9a68eee14b0a7dab22df8fd1f37900b92c
MilaTuzova <tuzovals@gmail.com>
2021-06-11 00:08:42 +0300

    corected click on empty

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -50,7 +50,7 @@ ul.insertAdjacentHTML('beforeend', galeryImages);
 
 
 function createdImagesCards(img) {
-
+    // console.log(img)
     return img.map(({ original, preview, description }) => {
 
         return `<li class="gallery__item">
@@ -70,7 +70,9 @@ function createdImagesCards(img) {
           </li>`;
     }).join('');
 
+
 };
+console.log(galeryImages);
 
 const ulContainer = document.querySelector("ul.js-gallery");
 const imgItems = document.querySelectorAll(".gallery__item");
@@ -88,7 +90,13 @@ function stopDefActive(e) {
 ulContainer.addEventListener('click', openModal);
 ulContainer.addEventListener('click', addBigImag);
 
-function openModal() {
+function openModal(evt) {
+    console.log(evt.target.dataset.source);
+
+    console.log(evt.target.src);
+    if (!evt.target.src) {
+        return
+    }
     modal.classList.add('is-open')
 };
 
